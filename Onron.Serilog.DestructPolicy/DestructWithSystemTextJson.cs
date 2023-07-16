53daefee70b7fc51df5dbb62a83b66f17b9397c3
Roni Stiawan <roni.physics@gmail.com>
2023-07-16 12:12:40 +0700

    add system text json as the serializer

---
+++ Onron.Serilog.DestructPolicy/DestructWithSystemTextJson.cs
@@ -0,0 +1,22 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Onron.Serilog.DestructPolicy
+{
+    public class DestructWithSystemTextJson : IDestructuringPolicy
+    {
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            ReferenceHandler = ReferenceHandler.IgnoreCycles
+        };
+
+        public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, out LogEventPropertyValue result)
+        {
+            result = new ScalarValue(JsonSerializer.Serialize(value, options));
+
+            return true;
+        }
+    }
+}
