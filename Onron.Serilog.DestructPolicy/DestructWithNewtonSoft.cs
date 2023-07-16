f280311a3281203f01767c19e004f9ec34ed1c1e
Roni Stiawan <roni.physics@gmail.com>
2023-07-16 12:04:17 +0700

    Add project files.

---
+++ Onron.Serilog.DestructPolicy/DestructWithNewtonSoft.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+
+namespace Onron.Serilog.DestructPolicy
+{
+    public class DestructWithNewtonSoft : IDestructuringPolicy
+    {
+        public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, out LogEventPropertyValue result)
+        {
+            result = new ScalarValue(JsonConvert.SerializeObject(value));
+            return true;
+        }
+    }
+}
